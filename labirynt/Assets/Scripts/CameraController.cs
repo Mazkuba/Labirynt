85e373fd0ecbfcd42db5676bfe894059ededb4a8
Mazkuba <kubixyt330@gmail.com>
2024-02-07 15:44:18 +0100

    f

--- labirynt/Assets/Scripts/CameraController.cs
+++ labirynt/Assets/Scripts/CameraController.cs
@@ -9,6 +9,8 @@ public class CameraController : MonoBehaviour
     float xRot = 0f;
     [SerializeField]
     Transform playerBody;
+    [SerializeField]
+    bool UpDownMovementEnabled;
 
     private void Start()
     {
@@ -22,13 +24,24 @@ public class CameraController : MonoBehaviour
 
     private void CameraRotation()
     {
+        //cos tam
+
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
         
         xRot -= mouseY;
         xRot = Mathf.Clamp(xRot, -90f, 90f);
-        //gora dol
-        transform.localRotation = Quaternion.Euler(xRot, 0, 0);
+        
+        if (UpDownMovementEnabled)
+        {
+            // góra i dó³
+            transform.localRotation = Quaternion.Euler(xRot, 0, 0);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        //lewo i prawo
         playerBody.transform.Rotate(new Vector3(0, mouseX, 0));
         
     }
--- labirynt/Assets/Scripts/PlayerController.cs
+++ labirynt/Assets/Scripts/PlayerController.cs
@@ -1,21 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     float speed;
     CharacterController characterController;
-    Rigidbody rb;
+    [SerializeField]
+    LayerMask groundLayer;
+    [SerializeField]
+    Transform groundChecker;
+    [SerializeField]
+    Text pointsUI;
+    [SerializeField]
+    private int points;
+
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
-        rb = GetComponent<Rigidbody>();
+      
     }
     private void Update()
     {
         PlayerMove();
+        ApplyPhysics();
+        ResetPlayer();
+        AddPointsUI();
+    }
+
+    void ResetPlayer()
+    {
+        if(transform.position.y <= -5f)
+        {
+            Debug.LogError("PLAYER IS OUT OF THE MAP");
+        }
     }
 
     private void PlayerMove()
@@ -24,7 +44,47 @@ public class PlayerController : MonoBehaviour
                z = Input.GetAxis("Vertical");
 
         //float y = transform.position.y;
-        Vector3 move = transform.right * x + transform.up * rb.velocity.y + transform.forward * z;
-        characterController.Move(move * speed);
+        Vector3 move = transform.right * x + transform.forward * z;
+        characterController.Move(move * speed * Time.deltaTime);
+
+        RaycastHit hit;
+        if(Physics.Raycast(groundChecker.position, Vector3.down, out hit, 0.4f, groundLayer))
+        {
+            switch (hit.transform.tag)
+            {
+                case "Low":
+                    speed = 1;
+                    break;
+                case "High":
+                    speed = 9;
+                    break;
+                default:
+                    speed = 5;
+                    break;
+
+            }
+        }
+      
+    }
+
+    void ApplyPhysics()
+    {
+        characterController.Move(Physics.gravity * Time.deltaTime);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.collider.tag == "Pickup")
+        {
+            hit.collider.GetComponent<Pickup>().Picked();
+            //Add point
+            points++;
+
+        }
+    }
+
+    void AddPointsUI()
+    {
+        pointsUI.text = $"Points: {points}";
     }
 }
